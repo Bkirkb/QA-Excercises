53d93902a4994df3e1f25aa0670e3dcbe3ff2fc3
Bkirkb <b.kirkby489@gmail.com>
2020-12-08 12:03:58 +0000

    GUI Form tutorial and excercise complete

--- templating/app.py
+++ templating/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField,StringField,SubmitField, DateField, IntegerField, DecimalField, SelectField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
 @app.route('/ben')
 def ben():
@@ -13,6 +16,36 @@ def harry():
 @app.route('/list')
 def listofnames():
     return render_template('listnames.html',listnames=["ben","harry","bob","jay","matt","bill"])
+
+class AddForm(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    today = DateField('Today is ?')
+    favnum = IntegerField('Your Favourite Number ?')
+    question = DecimalField('What is 5 - 2.5 ?')
+    validate = SelectField('Are you a Human ?', choices=["Yes", "No", "I'm not sure"])
+    submit = SubmitField('Add Name to Database')
+    
+@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+
+def home():
+    error=""
+    form= AddForm()
+
+    if request.method == 'POST':
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        today = form.today.data
+        favnum = form.favnum.data
+        question = form.question.data
+        validate = form.validate.data
+
+        if len(first_name) == 0 or len(last_name) == 0:
+            error = "Please supply both a first and last name"
+        else:
+            return 'Welcome ' + first_name + ' ' + last_name
+    return render_template('home.html', form=form, message=error)
 
 
 if __name__ == "__main__":
